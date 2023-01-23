4fc190bc9e636b23b3028635b4ae092591877e1c
Nikola Matošić <nikola.matosic2000@gmail.com>
2023-01-23 18:59:09 +0100

    Initial commit

---
+++ backend/src/main/java/com/matosic/PortalOtvorenihPodataka/PortalOtvorenihPodatakaApplication.java
@@ -0,0 +1,13 @@
+package com.matosic.PortalOtvorenihPodataka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalOtvorenihPodatakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalOtvorenihPodatakaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/matosic/PortalOtvorenihPodataka/PortalOtvorenihPodatakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matosic.PortalOtvorenihPodataka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalOtvorenihPodatakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
